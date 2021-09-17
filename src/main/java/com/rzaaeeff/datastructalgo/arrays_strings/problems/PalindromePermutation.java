68766fc2cc03c1e6a66d0f0cddf69a7baced01cf
Heydar Rzayev <rzaaeeff@gmail.com>
2021-09-17 08:17:43 +0400

    Solve "Palindrome Permutation"

--- src/main/java/com/rzaaeeff/datastructalgo/arrays_strings/problems/PalindromePermutation.java
+++ src/main/java/com/rzaaeeff/datastructalgo/arrays_strings/problems/PalindromePermutation.java
@@ -2,14 +2,14 @@ package com.rzaaeeff.datastructalgo.arrays_strings.problems;
 
 public class PalindromePermutation {
     public static void main(String[] args) {
-        if (palindromePermutation("tacos cat")) throw new AssertionError();
-        if (palindromePermutation("Tacot acosi")) throw new AssertionError();
-        if (!palindromePermutation("tacossocat")) throw new AssertionError();
-        if (!palindromePermutation("taco cat")) throw new AssertionError();
-        if (!palindromePermutation("Tact Coa")) throw new AssertionError();
+        if (palindromePermutation1("tacos cat")) throw new AssertionError();
+        if (palindromePermutation1("Tacot acosi")) throw new AssertionError();
+        if (!palindromePermutation1("tacossocat")) throw new AssertionError();
+        if (!palindromePermutation1("taco cat")) throw new AssertionError();
+        if (!palindromePermutation1("Tact Coa")) throw new AssertionError();
     }
 
-    private static boolean palindromePermutation(String s) {
+    private static boolean palindromePermutation0(String s) {
         int[] count = new int[32];
 
         for (int i = 0; i < s.length(); i++) {
@@ -31,5 +31,19 @@ public class PalindromePermutation {
         }
 
         return true;
+    }
+
+    private static boolean palindromePermutation1(String s) {
+        int checker = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) < 'A' || s.charAt(i) > 'z') continue;
+            int codePoint = s.charAt(i) - 'A';
+            if (codePoint > 31) codePoint -= 32;
+            int singleBitsOnPos = 1 << codePoint;
+            checker ^= singleBitsOnPos;
+        }
+
+        return checker == 0 || (checker & (checker - 1)) == 0;
     }
 }
